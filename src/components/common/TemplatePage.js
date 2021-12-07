59799ec29b50bd45a164d91e1687c5eccb7e420a
mark-xuereb <61869459+mark-xuereb@users.noreply.github.com>
2021-12-07 14:07:25 +0100

    Updated Template .html and .css
    I have updated the html and the css files for the template pages to be used. Mainly made the units of measurement more adaptive (em) and ordered them sequentially.

--- src/components/common/TemplatePage.js
+++ src/components/common/TemplatePage.js
@@ -1,52 +1,66 @@
 import './TemplatePage.css';
 
-function TemplatePage(){
-    return(
+function TemplatePage() {
+    return (
         <div className="screen">
-            <div className="buttons-section">
-                <a href="#" className="back-button">&lt;</a>
-                <a href="#" className="help-button">?</a>
-            </div>
-            <table style={{width:'75%'}}>
-            <tr>
-                <td style={{
-                    textAlign: 'left',
-                    width: '33%'
+            <table style={{ width: '75%' }}>
+                <tr>
+                    <td style={{
+                        textAlign: 'left',
+                        width: '33%'
                     }}>
-                    <small>
-                        [Patient Name]
-                    </small>
-                </td>
-                <td style={{
-                    textAlign: 'center',
-                    width: '33%'
+                        <label className="details">
+                            [Patient Name]
+                        </label>
+                    </td>
+                    <td style={{
+                        textAlign: 'center',
+                        width: '33%'
                     }}>
-                    <small>
-                        [Date]
-                    </small>
-                </td>
-                <td style={{
-                    textAlign: 'right',
-                    width: '33%'
+                        <label className="details">
+                            [Date]
+                        </label>
+                    </td>
+                    <td style={{
+                        textAlign: 'right',
+                        width: '33%'
                     }}>
-                    <small>
-                        [MR Name]
-                    </small>
-                </td>
-                
-            </tr>
+                        <label className="details">
+                            [MR Name]
+                        </label>
+                    </td>
+
+                </tr>
             </table>
-            <h1>Title</h1>
+            <div className="buttons-section space-between">
+                <a href="#" className="back-button">&lt;</a>
+                <label className="title">Sample Title</label>
+                <a href="#" className="help-button" style={{backgroundColor:'green'}}>?</a>
+            </div>
+            
             <div className="main-section">
-                <h2>Section Title</h2>
+                <label className="subtitle">Section Title</label>
                 <form>
-                    <input type="radio" id="test-radio-button" name="group1" value="value1"/>
-                    <label for="test-radio-button">Test Radio 1</label>
-                    <br/>
-                    <input type="radio" id="test-radio-button2" name="group1" value="value2"/>
-                    <label for="test-radio-button2">Test Radio 2</label>
+                    <div>
+                        <input type="radio" id="test-radio-button" name="group1" value="value1" />
+                        <label className="radio-button-label" for="test-radio-button">Test Radio 1</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="test-radio-button2" name="group1" value="value2" />
+                        <label className="radio-button-label" for="test-radio-button2">Test Radio 2</label>
+                    </div>
+                    <form>
+                        <div>
+                            <input type="radio" id="test-radio-button3" name="group2" value="value11" style={{ marginLeft: "7.5em" }} />
+                            <label className="radio-button-label" for="test-radio-button3">Indented Radio 1</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="test-radio-button4" name="group2" value="value12" style={{ marginLeft: "7.5em" }} />
+                            <label className="radio-button-label" for="test-radio-button4">Indented Radio 2</label>
+                        </div>
+                    </form>
                 </form>
-                
+
             </div>
             <a href="#" className="next-button">Next</a>
         </div>
