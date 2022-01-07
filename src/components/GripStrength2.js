13f68421f31fc62549cebc9cbaeac2acdf8642b3
LiamCurmideGray <liam.curmidegray.17@um.edu.mt>
2022-01-07 02:34:39 +0100

    Added some Database functionality:
    Including retreiving the Data and placing them in the db.
    Also Add a small temp button to homepage, (will probably need to be deleted)
    corrected firebase config
    
    Had to re adjust big time App.js so that the database can be utilised from any class

--- src/components/GripStrength2.js
+++ src/components/GripStrength2.js
@@ -1,22 +1,38 @@
 import './common/TemplatePage.css';
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './common/TemplatePage.css';
+import {useGripContext} from "./database/GripStrengthDatabase";
 
-class GripStrength2 extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
+
+const GripStrength2 = () => {
+   
+  const {GripStrengthResults2} = useGripContext();
+ const navigate = useNavigate();
+
+    const [question1, setAnswerQuestion1] = useState("");
+    const [question2, setAnswerQuestion2] = useState("");
+    const [question3, setAnswerQuestion3] = useState("");
+
+    console.log(question1);
+    console.log(question2);
+    console.log(question3);
+    console.log("\n");
+
+
+    function validateForm() {
+
+        console.log("Entered into the validate form");
+        if(question1 == "" || question2 == "" || question3 == "") {
+            console.log("Not all choices have been filled");
+            navigate("/GripStrength2")
+        } else {
+            console.log("Passing results to GripStrength");
+            GripStrengthResults2(question1,question2,question3);
+            navigate("/GripStrength3")
+        }
     }
 
-    handleChange = e => {
-        const { name, value } = e.target;
-        this.setState({
-            [name]: value
-        });
-        console.log(value);
-    };
-
-    render() {
         return (
             <div className="screen">
                 <table style={{ width: '75%' }}>
@@ -59,10 +75,10 @@ class GripStrength2 extends React.Component {
                         <ol>
                             <form>
                                 <li>Which hand do you use to sign your name? (This is the <b><u>dominant hand</u></b>)</li>
-                                <input type="radio" id="gst-question1-radio1" name="question1" value="right-hand" onChange={this.handleChange}/>
+                                <input type="radio" id="gst-question1-radio1" name="question1" value="sign name with right-hand" onChange={(e) =>setAnswerQuestion1({name: e.target.name, value: e.target.value})}/>
                                 <label className="radio-button-label" for="gst-question1-radio1">Right Hand</label>
                                 <br />
-                                <input type="radio" id="gst-question1-radio2" name="question1" value="left-hand" onChange={this.handleChange}/>
+                                <input type="radio" id="gst-question1-radio2" name="question1" value="sign name with left-hand" onChange={(e) => setAnswerQuestion1({name: e.target.name, value: e.target.value})}/>
                                 <label className="radio-button-label" for="gst-question1-radio2">Left Hand</label>
                             </form>
 
@@ -71,27 +87,33 @@ class GripStrength2 extends React.Component {
                                 <input type="radio" id="gst-question2-radio1" name="question2-yes-no" onClick={function () {
                                     var radio1 = document.getElementById("gst-question2-radio2");
                                     var radio2 = document.getElementById("gst-question2-radio3");
+                                    var radio4 = document.getElementById("gst-question2-radio4");
+                                    
 
                                     radio1.disabled = false;
                                     radio2.disabled = false;
+                                    radio4.checked = false;
                                 }} value="yes" />
                                 <label className="radio-button-label" for="gst-question2-radio1">Yes</label>
                                 <br />
-                                <input type="radio" id="gst-question2-radio2" name="question2-right-left" value="right-hand" disabled={true} style={{ marginLeft: "7.5em" }} />
+                                <input type="radio" id="gst-question2-radio2" name="question2" value="recent pain right-hand" disabled={true} style={{ marginLeft: "7.5em" }} onChange={(e) => setAnswerQuestion2({name: e.target.name, value: e.target.value})} />
                                 <label className="radio-button-label" for="gst-question2-radio2">Right Hand</label>
                                 <br />
-                                <input type="radio" id="gst-question2-radio3" name="question2-right-left" value="left-hand" disabled={true} style={{ marginLeft: "7.5em" }} />
+                                <input type="radio" id="gst-question2-radio3" name="question2" value="recent pain left-hand" disabled={true} style={{ marginLeft: "7.5em" }} onChange={(e) => setAnswerQuestion2({name: e.target.name, value: e.target.value})}/>
                                 <label className="radio-button-label" for="gst-question2-radio3">Left Hand</label>
                                 <br />
-                                <input type="radio" id="gst-question2-radio4" name="question2-yes-no" onClick={function () {
+                                <input type="radio" id="gst-question2-radio4" name="question2" onClick={function () {
                                     var radio1 = document.getElementById("gst-question2-radio2");
                                     var radio2 = document.getElementById("gst-question2-radio3");
+                                    var radio3 = document.getElementById("gst-question2-radio1");
 
                                     radio1.disabled = true;
                                     radio1.checked = false;
                                     radio2.disabled = true;
                                     radio2.checked = false;
-                                }} value="no" />
+                                    radio3.checked = false;
+
+                                }} value="no recent pain on either hand" onChange={(e) => setAnswerQuestion2({name: e.target.name, value: e.target.value})} />
                                 <label className="radio-button-label" for="gst-question2-radio4">No</label>
                                 <p><b>If yes to dominant hand, do not test.</b></p>
                             </form>
@@ -103,27 +125,31 @@ class GripStrength2 extends React.Component {
                                 <input type="radio" id="gst-question3-radio1" name="question3-yes-no" onClick={function () {
                                     var radio1 = document.getElementById("gst-question3-radio2");
                                     var radio2 = document.getElementById("gst-question3-radio3");
+                                    var radio4 = document.getElementById("gst-question3-radio4");
 
                                     radio1.disabled = false;
                                     radio2.disabled = false;
+                                    radio4.checked = false;
                                 }} value="yes" />
                                 <label className="radio-button-label" for="gst-question3-radio1">Yes</label>
                                 <br />
-                                <input type="radio" id="gst-question3-radio2" name="question3-right-left" value="right-hand" disabled={true} style={{ marginLeft: "7.5em" }} />
+                                <input type="radio" id="gst-question3-radio2" name="question3" value="yes recent surgery right-hand" disabled={true} style={{ marginLeft: "7.5em" }} onChange={(e) => setAnswerQuestion3({name: e.target.name, value: e.target.value})}/>
                                 <label className="radio-button-label" for="gst-question3-radio2">Right Hand</label>
                                 <br />
-                                <input type="radio" id="gst-question3-radio3" name="question3-right-left" value="left-hand" disabled={true} style={{ marginLeft: "7.5em" }} />
+                                <input type="radio" id="gst-question3-radio3" name="question3" value="yes recent surgery left-hand" disabled={true} style={{ marginLeft: "7.5em" }} onChange={(e) => setAnswerQuestion3({name: e.target.name, value: e.target.value})}/>
                                 <label className="radio-button-label" for="gst-question3-radio3">Left Hand</label>
                                 <br />
-                                <input type="radio" id="gst-question3-radio4" name="question3-yes-no" onClick={function () {
+                                <input type="radio" id="gst-question3-radio4" name="question3" onClick={function () {
                                     var radio1 = document.getElementById("gst-question3-radio2");
                                     var radio2 = document.getElementById("gst-question3-radio3");
+                                    var radio3 = document.getElementById("gst-question3-radio1");
 
                                     radio1.disabled = true;
                                     radio1.checked = false;
                                     radio2.disabled = true;
                                     radio2.checked = false;
-                                }} value="no" />
+                                    radio3.checked = false;
+                                }} value="no recent surgery in either hand" onChange={(e) => setAnswerQuestion3({name: e.target.name, value: e.target.value})}/>
                                 <label className="radio-button-label" for="gst-question3-radio4">No</label>
 
                                 <p><b>If yes to dominant hand, do not test.</b></p>
@@ -134,12 +160,9 @@ class GripStrength2 extends React.Component {
                         </ol>
                     </form>
                 </div>
-                <a href="/GripStrength3" className="next-button" onClick={this.validateForm}>Next</a>
+                <button className="next-button" onClick={validateForm}>Next</button>
             </div>
         );
-    }
-}
-function validateForm() {
+    };
 
-}
 export default GripStrength2;
