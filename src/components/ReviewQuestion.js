b45c88a10b6ee4a659f034904ad9d7f218764070
LiamCurmideGray <liam.curmidegray.17@um.edu.mt>
2022-01-08 22:30:30 +0100

    Partial Validation for ReviewQuestion.js

--- src/components/ReviewQuestion.js
+++ src/components/ReviewQuestion.js
@@ -1,40 +1,38 @@
 import './common/TemplatePage.css';
-
-
-var group;
+import CommonHeader from './common/CommonHeader';
+import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 
 function ReviewQuestion() {
+
+    const [question1, setAnswerQuestion1] = useState("");
+    const navigate = useNavigate();
+
+    console.log(question1);
+
+    function validateForm(){
+        if (question1 == "") {
+            document.getElementById("question1-alert").innerHTML = "Please select an option!";
+            document.getElementById("question1Form").scrollIntoView();
+        }
+        else if (question1 == "other" || question1 == "independent-and-walking-aid with "){
+            document.getElementById("question1-other-alert").innerHTML = "Please input a reason!";
+            document.getElementById("question1Form").scrollIntoView();
+        }
+        else {
+            document.getElementById("question1-alert").innerHTML = "";
+            document.getElementById("question1-other-alert").innerHTML = "";
+        }
+
+        if (question1 != "" && question1 != "other" ) {
+           
+            sessionStorage.setItem("TUGTestCarriedOut", "Previous Level of Mobility " + question1);
+            navigate("/");
+        }
+    }
     return (
         <div className="screen">
-            <table style={{ width: '75%' }}>
-                <tr>
-                    <td style={{
-                        textAlign: 'left',
-                        width: '33%'
-                    }}>
-                        <label className="details">
-                            [Patient Name]
-                        </label>
-                    </td>
-                    <td style={{
-                        textAlign: 'center',
-                        width: '33%'
-                    }}>
-                        <label className="details">
-                            [Date]
-                        </label>
-                    </td>
-                    <td style={{
-                        textAlign: 'right',
-                        width: '33%'
-                    }}>
-                        <label className="details">
-                            [MR Name]
-                        </label>
-                    </td>
-
-                </tr>
-            </table>
+           {CommonHeader()}
             <div className="buttons-section space-between">
                 <a href="/RiskOfFallStatus" className="back-button">&lt;</a>
                 <label className="title">Timed Up and Go Test</label>
@@ -45,68 +43,60 @@ function ReviewQuestion() {
                 <label className="subtitle">TUG Test Carried out</label>
                 <form>
                     <div>
-                        <input type="radio" id="radio-button-yes" name="group1" value="yes" onChange={disableButton()} />
+                        <input type="radio" id="radio-button-yes" name="group1" value="TUG Test Carried out: Yes" onChange={(e) => setAnswerQuestion1(e.target.value)}
+                        onClick={function () {
+                            document.getElementById("TUGTestCarriedOut").disabled = true;
+                            document.getElementById("radio-button-att").checked = false;
+                            document.getElementById("radio-button-unsafe").checked = false;
+                            document.getElementById("radio-button-unable").checked = false;
+                            document.getElementById("radio-button-refused").checked = false;
+                            document.getElementById("radio-button-prev-other").checked = false;
+                        }} />
                         <label className="radio-button-label" for="radio-button-yes">Yes</label>
                     </div>
                     <div>
-                        <input type="radio" id="radio-button-no" name="group1" value="no" onChange={enableButton()} />
+                        <input type="radio" id="radio-button-no" name="group1" value="no"
+                        onClick={function () {
+                            document.getElementById("TUGTestCarriedOut").disabled = false; }}/>
+
                         <label className="radio-button-label" for="radio-button-no">No</label>
                     </div>
-                    <form>
+                    <fieldset id="TUGTestCarriedOut" disabled={true}>
                         <div>
-                            <input type="radio" id="radio-button-att" name="group1-1" value="attempted-but-unable" style={{ marginLeft: "7.5em" }} />
+                            <input type="radio" id="radio-button-att" name="group1-1" value="TUG Test Carried out: No, Reason: attempted-but-unable" style={{ marginLeft: "7.5em" }} 
+                            onChange={(e) => setAnswerQuestion1(e.target.value)}/>
                             <label className="radio-button-label" for="radio-button-att">Attempted, but unable</label>
                         </div>
                         <div>
-                            <input type="radio" id="radio-button-unsafe" name="group1-1" value="unsafe" style={{ marginLeft: "7.5em" }} />
+                            <input type="radio" id="radio-button-unsafe" name="group1-1" value="TUG Test Carried out: No, Reason: unsafe" style={{ marginLeft: "7.5em" }} 
+                            onChange={(e) => setAnswerQuestion1(e.target.value)}/>
                             <label className="radio-button-label" for="radio-button-unsafe">Unsafe</label>
                         </div>
                         <div>
-                            <input type="radio" id="radio-button-unable" name="group1-1" value="unable" style={{ marginLeft: "7.5em" }} />
+                            <input type="radio" id="radio-button-unable" name="group1-1" value="TUG Test Carried out: No, Reason: unable" style={{ marginLeft: "7.5em" }} 
+                            onChange={(e) => setAnswerQuestion1(e.target.value)}/>
                             <label className="radio-button-label" for="radio-button-unable">Unable to understand command</label>
                         </div>
                         <div>
-                            <input type="radio" id="radio-button-refused" name="group1-1" value="refused" style={{ marginLeft: "7.5em" }} />
+                            <input type="radio" id="radio-button-refused" name="group1-1" value="TUG Test Carried out: No, Reason: refused" style={{ marginLeft: "7.5em" }} 
+                            onChange={(e) => setAnswerQuestion1(e.target.value)}/>
                             <label className="radio-button-label" for="radio-button-refused">Refused</label>
                         </div>
                         <div>
-                            <input type="radio" id="radio-button-prev-other" name="group1-1" value="other" style={{ marginLeft: "7.5em" }} />
-                            <label className="radio-button-label" for="radio-button-prev-other">Other</label>
+                            <input type="radio" id="radio-button-prev-other" name="group1-1" value="other" style={{ marginLeft: "7.5em" }} onClick={function () {
+                                    document.getElementById("left-hand-text-box").disabled = false;
+                                }}  onChange={(e) => setAnswerQuestion1(e.target.value)} style={{ marginLeft: "7.5em" }} />
+                            <label className="radio-button-label" for="radio-button-prev-other">Other:</label>
+                            <input type="text" id="left-hand-text-box" disabled={true} onBlur={(e) => setAnswerQuestion1("TUG Test Carried out: No, Reason: " + e.target.value)} />
+                            <h3 class="alert" id="question1-other-alert"></h3>
+
                         </div>
-                    </form>
+                    </fieldset>
                 </form>
             </div>
-            <a href="/" className="next-button">Complete TUG Test</a>
+            <button className="next-button" onClick={validateForm}>Complete TUG Test</button>
         </div>
     );
-}
-
-function disableButton(id) {
-
-    if (id === "radio-button-prev-ind-and-unaid") {
-        group = document.getElementsByName("group1-1");
-    }
-    else {
-        group = document.getElementsByName("group1-2");
-    }
-
-    for (var i = 0; i < group.length - 1; i++) {
-        group[i].disabled = true;
-    }
-}
-
-function enableButton(id) {
-
-    if (id === "radio-button-prev-ind-and-unaid") {
-        group = document.getElementsByName("group1-1");
-    }
-    else {
-        group = document.getElementsByName("group1-2");
-    }
-
-    for (var i = 0; i < group.length - 1; i++) {
-        group[i].disabled = false;
-    }
 }
 
 export default ReviewQuestion;
