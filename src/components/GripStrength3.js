f93da42f9aaa284b67e69d28d8ad5f3a87684aa2
LiamCurmideGray <liam.curmidegray.17@um.edu.mt>
2022-01-08 01:19:41 +0100

    Grip Strength Classes, all have according validation and once the Test in completed, it is saved to the database

--- src/components/GripStrength3.js
+++ src/components/GripStrength3.js
@@ -1,18 +1,75 @@
 import "./common/TemplatePage.css";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import { useState } from "react";
-
-function maxVal(input, input2) {
-  const values = [input, input2];
-  const LeftMax = Math.max(...values);
-  return LeftMax;
-}
+import { useNavigate } from 'react-router-dom';
+import {Alert} from "react-bootstrap";
 
 function LeftResultInputs() {
-  const [input1, setInput1] = useState("");
-  const [input2, setInput2] = useState("");
+  const [leftInput1, setLeftInput1] = useState(0);
+  const [leftInput2, setLeftInput2] = useState(0);
+  const [rightInput1, setRightInput1] = useState(0);
+  const [rightInput2, setRightInput2] = useState(0);
+  const [error, setError] = useState("Please fill the fields for both hands, or only fill for one of the hands");
+  const [errorConfirm, setErrorConfirm] = useState(false);
+  sessionStorage.setItem("MaxLeftHandResult", "No Left Result");
+  sessionStorage.setItem("MaxRightHandResult","No Right Result");
+
+
+  const navigate = useNavigate();
+
+  function maxVal(input, input2) {
+
+    const values = [input, input2];
+    const LeftMax = Math.max(...values);
+    return LeftMax;
+  }
+  
+  function onSubmit(){
+
+    if(errorConfirm == true) {
+    navigate("/GripStrength4")
+    }
+    setError("");
+
+    if((leftInput1 == 0 || leftInput2 == 0) && (rightInput1 == 0 || rightInput2 == 0)) {
+      setError("You're about to proceed without inputting anything, click next button again to proceed without any values");
+      setErrorConfirm(true);
+      }
+
+    else {
+      if(leftInput1 == 0 || leftInput2 == 0){
+        sessionStorage.setItem("MaxLeftHandResult", "No Left Result");
+      }  
+      else {
+
+        if(rightInput1 == 0 || rightInput2 == 0){
+
+          setError("You haven't inputed the data for the Right hand!, click next button again to proceed without Right Hand values");
+          setErrorConfirm(true);
+        }
+        sessionStorage.setItem("MaxLeftHandResult"  ,`Left Max Result: ${maxVal(leftInput1, leftInput2)}kg`);
+      }
+
+      if(rightInput1 == 0 || rightInput2 == 0){
+        sessionStorage.setItem("MaxRightHandResult","No Right Result");
+       } 
+       else {
+
+        if(leftInput1 == 0 || leftInput2 == 0){
+          setError("You haven't inputed the data for the Left hand!, click next button again to proceed without Left Hand values");
+          setErrorConfirm(true);
+        }
+         sessionStorage.setItem("MaxRightHandResult"  ,`Right Max Result: ${maxVal(rightInput1, rightInput2)}kg`);
+       }
+
+       if(sessionStorage.getItem("MaxLeftHandResult") != "No Left Result" && sessionStorage.getItem("MaxRightHandResult") != "No Right Result"){
+        navigate("/GripStrength4")
+       }
+  }
+  
+  }
 
   return (
     <div className="screen">
@@ -55,6 +112,7 @@ function LeftResultInputs() {
       </div>
 
       <div className="main-section">
+      {error && <Alert  variant="danger">{error}</Alert>}
         <label className="subtitle">Left Hand Results</label>
         <form>
           <table style={{ width: "100%" }}>
@@ -73,7 +131,7 @@ function LeftResultInputs() {
                   label="Required"
                   type="number"
                   variant="filled"
-                  onChange={(event) => setInput1(event.target.value)}
+                  onChange={(event) => {setLeftInput1(event.target.value); setErrorConfirm(false);}}
                   InputProps={{
                     startAdornment: (
                       <InputAdornment position="start">kg</InputAdornment>
@@ -101,7 +159,7 @@ function LeftResultInputs() {
                   label="Required"
                   type="number"
                   variant="filled"
-                  onChange={(event) => setInput2(event.target.value)}
+                  onChange={(event) => {setLeftInput2(event.target.value); setErrorConfirm(false);}}
                   InputProps={{
                     startAdornment: (
                       <InputAdornment position="start">kg</InputAdornment>
@@ -126,14 +184,6 @@ function LeftResultInputs() {
                   width: "33%",
                 }}
               >
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    maxVal(input1, input2);
-                  }}
-                >
-                  Save Maximum Value
-                </Button>
               </td>
               <td
                 style={{
@@ -162,7 +212,7 @@ function LeftResultInputs() {
                   label="Required"
                   type="number"
                   variant="filled"
-                  onChange={(event) => setInput1(event.target.value)}
+                  onChange={(event) => {setRightInput1(event.target.value); setErrorConfirm(false);}}
                   InputProps={{
                     startAdornment: (
                       <InputAdornment position="start">kg</InputAdornment>
@@ -190,7 +240,7 @@ function LeftResultInputs() {
                   label="Required"
                   type="number"
                   variant="filled"
-                  onChange={(event) => setInput2(event.target.value)}
+                  onChange={(event) => {setRightInput2(event.target.value); setErrorConfirm(false);}}
                   InputProps={{
                     startAdornment: (
                       <InputAdornment position="start">kg</InputAdornment>
@@ -216,14 +266,6 @@ function LeftResultInputs() {
                   width: "33%",
                 }}
               >
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    maxVal(input1, input2);
-                  }}
-                >
-                  Save Maximum Value
-                </Button>
               </td>
               <td
                 style={{
@@ -235,9 +277,8 @@ function LeftResultInputs() {
           </table>
         </form>
       </div>
-      <a href="/GripStrength4" className="next-button">
-        Next
-      </a>
+      <button className="next-button" onClick={onSubmit}>Next</button>
+
     </div>
   );
 }
