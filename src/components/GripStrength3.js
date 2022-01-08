b81b1f98a5aec3de1d091457602142162f3c2581
stefsche <58699420+stefsche@users.noreply.github.com>
2022-01-08 22:30:15 +0100

    full gst validation

--- src/components/GripStrength3.js
+++ src/components/GripStrength3.js
@@ -2,11 +2,11 @@ import "./common/TemplatePage.css";
 // import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, Navigate } from 'react-router-dom';
 import { Alert } from "react-bootstrap";
 
-function GstResultInputs() {
+const GripStrength3 = () => {
   const [leftInput1, setLeftInput1] = useState(0);
   const [leftInput2, setLeftInput2] = useState(0);
   const [rightInput1, setRightInput1] = useState(0);
@@ -15,18 +15,47 @@ function GstResultInputs() {
   const [errorLeft, setErrorLeft] = useState("");
   const [errorRight, setErrorRight] = useState("");
   const [errorConfirm, setErrorConfirm] = useState(false);
-  var question1 = sessionStorage.getItem("question1");
-  var question2 = sessionStorage.getItem("question2");
-  var question3 = sessionStorage.getItem("question3");
+  const question1 = sessionStorage.getItem("question1");
+  const question2 = sessionStorage.getItem("question2");
+  const question3 = sessionStorage.getItem("question3");
   sessionStorage.setItem("MaxLeftHandResult", "No Left Result");
   sessionStorage.setItem("MaxRightHandResult", "No Right Result");
 
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    if (question2 == "recent pain right-hand" || question3 == "yes recent surgery right-hand") {
+      document.getElementById("rightHandFieldset").hidden = true;
+      setErrorRight("Results disabled due to recent pain or surgery in right hand");
+    }
+    if (question2 == "recent pain left-hand" || question3 == "yes recent surgery left-hand") {
+      document.getElementById("leftHandFieldset").hidden = true;
+      setErrorLeft("Results disabled due to recent pain or surgery in left hand");
+    }
+    if (question1 == "sign name with right-hand") {
+      if (question2 == "recent pain right-hand" || question3 == "yes recent surgery right-hand") {
+        document.getElementById("rightHandFieldset").hidden = true;
+        document.getElementById("leftHandFieldset").hidden = true;
+        setErrorRight("Results disabled due to recent pain or surgery in dominant hand");
+        setErrorLeft("Results disabled due to recent pain or surgery in dominant hand");
+      }
+    }
+    if (question1 == "sign name with left-hand") {
+      if (question2 == "recent pain left-hand" || question3 == "yes recent surgery left-hand") {
+        document.getElementById("rightHandFieldset").hidden = true;
+        document.getElementById("leftHandFieldset").hidden = true;
+        setErrorRight("Results disabled due to recent pain or surgery in dominant hand");
+        setErrorLeft("Results disabled due to recent pain or surgery in dominant hand");
+      }
+    }
+  });
+
   if (question1 == "" || question2 == "" || question3 == "") {
-    return <Navigate to="/GripStrength2"/>;
+    return <Navigate to="/GripStrength2" />;
   }
+
+
   function maxVal(input, input2) {
 
     const values = [input, input2];
@@ -78,32 +107,34 @@ function GstResultInputs() {
   return (
     <div className="screen">
       <table style={{ width: "75%" }}>
-        <tr>
-          <td
-            style={{
-              textAlign: "left",
-              width: "33%",
-            }}
-          >
-            <label className="details">[Patient Name]</label>
-          </td>
-          <td
-            style={{
-              textAlign: "center",
-              width: "33%",
-            }}
-          >
-            <label className="details">[Date]</label>
-          </td>
-          <td
-            style={{
-              textAlign: "right",
-              width: "33%",
-            }}
-          >
-            <label className="details">[MR Name]</label>
-          </td>
-        </tr>
+        <tbody>
+          <tr>
+            <td
+              style={{
+                textAlign: "left",
+                width: "33%",
+              }}
+            >
+              <label className="details">[Patient Name]</label>
+            </td>
+            <td
+              style={{
+                textAlign: "center",
+                width: "33%",
+              }}
+            >
+              <label className="details">[Date]</label>
+            </td>
+            <td
+              style={{
+                textAlign: "right",
+                width: "33%",
+              }}
+            >
+              <label className="details">[MR Name]</label>
+            </td>
+          </tr>
+        </tbody>
       </table>
       <div className="buttons-section space-between">
         <a href="/GripStrength2" className="back-button">
@@ -121,170 +152,181 @@ function GstResultInputs() {
         {error && <Alert variant="danger">{error}</Alert>}
         <label className="subtitle">Left Hand Results</label>
         <form>
+          <fieldset id="leftHandFieldset">
+            <table style={{ width: "100%" }}>
+              <tbody>
+                <tr>
+                  <td
+                    style={{
+                      textAlign: "left",
+                      width: "33%",
+                    }}
+                  >
+                    <label>1st Reading</label>
+                    <TextField
+                      fullWidth
+                      id="FirstReading"
+                      className="gripInputs"
+                      label="Required"
+                      type="number"
+                      variant="filled"
+                      onChange={(event) => { setLeftInput1(event.target.value); setErrorConfirm(false); }}
+                      InputProps={{
+                        inputProps: { min: 0 },
+                        startAdornment: (
+                          <InputAdornment position="start">kg</InputAdornment>
+                        ),
+                        inputMode: 'numeric',
+                      }}
+                    />
+                  </td>
+                  <td
+                    style={{
+                      textAlign: "center",
+                      width: "33%",
+                    }}
+                  ></td>
+                  <td
+                    style={{
+                      textAlign: "right",
+                      width: "33%",
+                    }}
+                  >
+                    <label>2nd Reading</label>
+                    <TextField
+                      id="Second Reading"
+                      className="gripInputs"
+                      label="Required"
+                      type="number"
+                      variant="filled"
+                      onChange={(event) => { setLeftInput2(event.target.value); setErrorConfirm(false); }}
+                      InputProps={{
+                        inputProps: { min: 0 },
+                        startAdornment: (
+                          <InputAdornment position="start">kg</InputAdornment>
+                        ),
+                        inputMode: 'numeric',
+                      }}
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </fieldset>
           <table style={{ width: "100%" }}>
-            <tr>
-              <td
-                style={{
-                  textAlign: "left",
-                  width: "33%",
-                }}
-              >
-                <label>1st Reading</label>
-                <TextField
-                  fullWidth
-                  id="FirstReading"
-                  className="gripInputs"
-                  label="Required"
-                  type="number"
-                  variant="filled"
-                  onChange={(event) => { setLeftInput1(event.target.value); setErrorConfirm(false); }}
-                  InputProps={{
-                    inputProps: { min: 0 },
-                    startAdornment: (
-                      <InputAdornment position="start">kg</InputAdornment>
-                    ),
-                    inputMode: 'numeric',
+            <tbody>
+              <tr>
+                <td
+                  style={{
+                    textAlign: "left",
+                    width: "33%",
                   }}
-                />
-              </td>
-              <td
-                style={{
-                  textAlign: "center",
-                  width: "33%",
-                }}
-              ></td>
-              <td
-                style={{
-                  textAlign: "right",
-                  width: "33%",
-                }}
-              >
-                <label>2nd Reading</label>
-                <TextField
-                  id="Second Reading"
-                  className="gripInputs"
-                  label="Required"
-                  type="number"
-                  variant="filled"
-                  onChange={(event) => { setLeftInput2(event.target.value); setErrorConfirm(false); }}
-                  InputProps={{
-                    inputProps: { min: 0 },
-                    startAdornment: (
-                      <InputAdornment position="start">kg</InputAdornment>
-                    ),
-                    inputMode: 'numeric',
+                ></td>
+                <td
+                  style={{
+                    textAlign: "center",
+                    width: "33%",
                   }}
-                />
-              </td>
-            </tr>
-          </table>
-          <table style={{ width: "100%" }}>
-            <tr>
-              <td
-                style={{
-                  textAlign: "left",
-                  width: "33%",
-                }}
-              ></td>
-              <td
-                style={{
-                  textAlign: "center",
-                  width: "33%",
-                }}
-              >
-              </td>
-              <td
-                style={{
-                  textAlign: "right",
-                  width: "33%",
-                }}
-              ></td>
-            </tr>
+                >
+                </td>
+                <td
+                  style={{
+                    textAlign: "right",
+                    width: "33%",
+                  }}
+                ></td>
+              </tr>
+            </tbody>
           </table>
           {errorLeft && <Alert variant="danger">{errorLeft}</Alert>}
 
           <h1 className="subtitle">Right Hand Results</h1>
 
+          <fieldset id="rightHandFieldset">
+            <table style={{ width: "100%" }}>
+              <tbody>
+                <tr>
+                  <td
+                    style={{
+                      textAlign: "left",
+                      width: "33%",
+                    }}
+                  >
+                    <label>1st Reading</label>
+                    <TextField
+                      fullWidth
+                      id="FirstReading"
+                      className="gripInputs"
+                      label="Required"
+                      type="number"
+                      variant="filled"
+                      onChange={(event) => { setRightInput1(event.target.value); setErrorConfirm(false); }}
+                      InputProps={{
+                        inputProps: { min: 0 },
+                        startAdornment: (
+                          <InputAdornment position="start">kg</InputAdornment>
+                        ),
+                        inputMode: 'numeric',
+                      }}
+                    />
+                  </td>
+                  <td
+                    style={{
+                      textAlign: "center",
+                      width: "33%",
+                    }}
+                  ></td>
+                  <td
+                    style={{
+                      textAlign: "right",
+                      width: "33%",
+                    }}
+                  >
+                    <label>2nd Reading</label>
+                    <TextField
+                      id="Second Reading"
+                      className="gripInputs"
+                      label="Required"
+                      type="number"
+                      variant="filled"
+                      onChange={(event) => { setRightInput2(event.target.value); setErrorConfirm(false); }}
+                      InputProps={{
+                        inputProps: { min: 0 },
+                        startAdornment: (
+                          <InputAdornment position="start">kg</InputAdornment>
+                        ),
+                        inputMode: 'numeric',
+                      }}
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </fieldset>
           <table style={{ width: "100%" }}>
-            <tr>
-              <td
-                style={{
-                  textAlign: "left",
-                  width: "33%",
-                }}
-              >
-                <label>1st Reading</label>
-                <TextField
-                  fullWidth
-                  id="FirstReading"
-                  className="gripInputs"
-                  label="Required"
-                  type="number"
-                  variant="filled"
-                  onChange={(event) => { setRightInput1(event.target.value); setErrorConfirm(false); }}
-                  InputProps={{
-                    inputProps: { min: 0 },
-                    startAdornment: (
-                      <InputAdornment position="start">kg</InputAdornment>
-                    ),
-                    inputMode: 'numeric',
+            <tbody>
+              <tr>
+                <td
+                  style={{
+                    textAlign: "left",
+                    width: "33%",
                   }}
-                />
-              </td>
-              <td
-                style={{
-                  textAlign: "center",
-                  width: "33%",
-                }}
-              ></td>
-              <td
-                style={{
-                  textAlign: "right",
-                  width: "33%",
-                }}
-              >
-                <label>2nd Reading</label>
-                <TextField
-                  id="Second Reading"
-                  className="gripInputs"
-                  label="Required"
-                  type="number"
-                  variant="filled"
-                  onChange={(event) => { setRightInput2(event.target.value); setErrorConfirm(false); }}
-                  InputProps={{
-                    inputProps: { min: 0 },
-                    startAdornment: (
-                      <InputAdornment position="start">kg</InputAdornment>
-                    ),
-                    inputMode: 'numeric',
+                ></td>
+                <td
+                  style={{
+                    textAlign: "center",
+                    width: "33%",
                   }}
-                />
-              </td>
-            </tr>
-          </table>
-
-          <table style={{ width: "100%" }}>
-            <tr>
-              <td
-                style={{
-                  textAlign: "left",
-                  width: "33%",
-                }}
-              ></td>
-              <td
-                style={{
-                  textAlign: "center",
-                  width: "33%",
-                }}
-              >
-              </td>
-              <td
-                style={{
-                  textAlign: "right",
-                  width: "33%",
-                }}
-              ></td>
-            </tr>
+                >
+                </td>
+                <td
+                  style={{
+                    textAlign: "right",
+                    width: "33%",
+                  }}
+                ></td>
+              </tr>
+            </tbody>
           </table>
           {errorRight && <Alert variant="danger">{errorRight}</Alert>}
         </form>
@@ -295,5 +337,5 @@ function GstResultInputs() {
   );
 }
 
-export default GstResultInputs;
+export default GripStrength3;
 
